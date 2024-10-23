61dd27cb1d0f3a9e668c9722adfd68f4d48a8d1b
Tim te Beek <tim@moderne.io>
2024-10-23 12:24:40 +0200

    Repeat removal of unused local variables if needed

--- src/test/java/org/openrewrite/featureflags/launchdarkly/RemoveBoolVariationTest.java
+++ src/test/java/org/openrewrite/featureflags/launchdarkly/RemoveBoolVariationTest.java
@@ -246,12 +246,10 @@ class RemoveBoolVariationTest implements RewriteTest {
               }
               """,
             """
-              import com.launchdarkly.sdk.*;
               import com.launchdarkly.sdk.server.*;
               class Foo {
                   LDClient client = new LDClient("sdk-key-123abc");
                   void bar() {
-                      LDContext ldContext = LDContext.create("newValue");
                       // Application code to show the feature
                       System.out.println("Feature is on");
                   }
