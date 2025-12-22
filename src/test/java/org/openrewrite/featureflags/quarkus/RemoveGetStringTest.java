cd4333d325db0c4526f0f28f210cd2e5b9bb68ff
Tim te Beek <tim@moderne.io>
2025-12-22 14:50:10 +0100

    Add `RemoveGetString` and `RemoveGetInt`

---
+++ src/test/java/org/openrewrite/featureflags/quarkus/RemoveGetStringTest.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2025 the original author or authors.
+ * <p>
+ * Licensed under the Moderne Source Available License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://docs.moderne.io/licensing/moderne-source-available-license
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.featureflags.quarkus;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.DocumentExample;
+import org.openrewrite.java.JavaParser;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+import static org.openrewrite.java.Assertions.java;
+
+class RemoveGetStringTest implements RewriteTest {
+
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec.recipe(new RemoveGetString("flag-key-123abc", "production"))
+          .parser(JavaParser.fromJavaVersion().classpath("quarkus-flags"));
+    }
+
+    @DocumentExample
+    @Test
+    void removeGetString() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              import io.quarkiverse.flags.Flags;
+
+              class Foo {
+                  void bar(Flags flags) {
+                      String environment = flags.getString("flag-key-123abc");
+                      if ("production".equals(environment)) {
+                          System.out.println("Production mode");
+                      } else {
+                          System.out.println("Default mode");
+                      }
+                  }
+              }
+              """,
+            """
+              import io.quarkiverse.flags.Flags;
+
+              class Foo {
+                  void bar(Flags flags) {
+                      System.out.println("Production mode");
+                  }
+              }
+              """
+          )
+        );
+    }
+}
